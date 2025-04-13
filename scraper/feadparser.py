2e1266ddffa27a7d5f8115a05787879018ff1472
avery-hankins <164415993+avery-hankins@users.noreply.github.com>
2025-04-12 21:29:37 -0400

    feedparser update

--- scraper/feadparser.py
+++ scraper/feadparser.py
@@ -8,7 +8,14 @@ for url in urls:
     if not url:
         continue
     feed = feedparser.parse(url)
+
     print("Feed Title:", feed.feed.get("title", "N/A"))
     print("Feed Description:", feed.feed.get("description", "N/A"))
     print("Feed Link:", feed.feed.get("link", "N/A"))
     print("-" * 40)
+
+    for entry in feed.entries:
+        print(" Title:", entry.get("title", "N/A"))
+        print(" Link:", entry.get("link", "N/A"))
+        print(" Summary:", entry.get("summary", "N/A"))
+        print("-" * 40)
