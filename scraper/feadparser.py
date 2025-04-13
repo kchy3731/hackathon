b2e40a591319f1b395162982f9eb72bf1683de2a
PrithviVegesna <vegesnaprithvi@gmail.com>
2025-04-12 18:08:42 -0700

    Hello

--- scraper/feadparser.py
+++ scraper/feadparser.py
@@ -3,6 +3,10 @@ import feedparser
 url = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
 feed = feedparser.parse(url)
 
-print("Feed Title:", feed.feed.title)
-print("Feed Description:", feed.feed.description)
-print("Feed Link:", feed.feed.link)
+for entry in feed.entries:
+   print("Entry Title:", entry.title)
+   print("Entry Link:", entry.link)
+   print("Entry Published Date:", entry.published)
+   print("Entry Summary:", entry.summary)
+   print("\n")
+
