210746cb2a19292a4c34e6cda7942c36e551ea29
PrithviVegesna <vegesnaprithvi@gmail.com>
2025-04-12 20:31:18 -0700

    commited

--- scraper/feadparser.py
+++ scraper/feadparser.py
@@ -1,9 +1,8 @@
 import feedparser
 from datetime import datetime, timedelta
-
+from bf4 import beautifulsoup
+import requests
 import csv
-
-
 
 now = datetime.now()
 time_range = timedelta(days=1)
@@ -25,13 +24,27 @@ for url in urls:
         last_plus = (entry.published).rfind("+")
         if last_plus != -1:
             corrected_date_str = entry.published[:last_plus] + "-" + entry.published[last_plus + 1:]
-            entry_date = datetime.strptime(corrected_date_str, "%Y-%m-%dT%H:%M:%S-00:00")
+            entry_date = datetime.strptime((corrected_date_str), "%a, %d %b %Y %H:%M:%S %z")
             if now - entry_date <= time_range:
                 print("Entry Title:", entry.title)
                 print("Entry Link:", entry.link)
                 print("Entry Published Date:", entry.published)
                 print("Entry Summary:", entry.summary)
                 print("Entry Description:", entry.description)
+                if (".youtube.com") in entry:
+                    #code for youtube transcript
+                    print("Video Transcript:", entry.description)
+                else:
+
+
+
+                    response = requests.get(entry.link)
+
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content, "html.parser") 
+                        print(soup.get_text())
+
+                    print("Entry Content:", Transcript)
                 print("\n")
 
     with open('rss_data.csv', mode='w', newline='', encoding='utf-8') as csv_file:
